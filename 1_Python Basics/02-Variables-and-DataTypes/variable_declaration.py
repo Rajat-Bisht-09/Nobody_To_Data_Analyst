f9350d14b6be5dab0ed5ea86136afb1217b2a1c4
Rajat Bisht <81902672+Rajat-Bisht-09@users.noreply.github.com>
2025-01-09 19:28:28 +0530

    Update variable_declaration.py

--- 1_Python Basics/02-Variables-and-DataTypes/variable_declaration.py
+++ 1_Python Basics/02-Variables-and-DataTypes/variable_declaration.py
@@ -16,3 +16,19 @@ print(a,b,c)
 ```We can assign different values to multiple variables simultaneously, making the code concise and easier to read.```
 x, y, z = 1, 2.5, "Python"
 print(x, y, z)
+
+# Define variables with different data types
+n = 42
+f = 3.14
+s = "Hello, World!"
+li = [1, 2, 3]
+d = {'key': 'value'}
+bool = True
+
+# Get and print the type of each variable
+print(type(n))   
+print(type(f)) 
+print(type(s))   
+print(type(li))     
+print(type(d))     
+print(type(bool))
