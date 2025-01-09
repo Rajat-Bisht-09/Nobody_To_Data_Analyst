48fe4c8baba7c4aa12c0c4459ad3ade6864883a4
Rajat Bisht <81902672+Rajat-Bisht-09@users.noreply.github.com>
2025-01-09 19:20:28 +0530

    Update variable_declaration.py

--- 1_Python Basics/02-Variables-and-DataTypes/variable_declaration.py
+++ 1_Python Basics/02-Variables-and-DataTypes/variable_declaration.py
@@ -6,3 +6,13 @@ is_student = True
 print("Name:", name)
 print("Age:", age)
 print("Is Student:", is_student)
+
+# Multiple Assignments
+``` Python allows assigning the same value to multiple variables in a single line, which can be useful for initializing variables with the same value. ```
+a=b=c=100
+print(a,b,c)
+
+# Assigning different values
+```We can assign different values to multiple variables simultaneously, making the code concise and easier to read.```
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
